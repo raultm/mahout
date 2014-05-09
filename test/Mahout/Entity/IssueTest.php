c1639cec5482969ef586d4dcdf2f80a9ead5eb51
Raul Tierno <raultm@gmail.com>
2014-05-09 21:42:16 +0200

    Testsuite - Replace Tabs for spaces

--- test/Mahout/Entity/IssueTest.php
+++ test/Mahout/Entity/IssueTest.php
@@ -4,31 +4,31 @@ namespace Mahout\Entity;
 
 class IssueTest extends \PHPUnit_Framework_TestCase
 {
-	public function getJsonIssues(){
-		$data = [
-			[file_get_contents('./responses/issue14815.json'), "14815", "PLAN-572", "Cálculo correcto del VAN"],
-		];
-		return $data;
-	}
-	
-	/**
-	* @dataProvider getJsonIssues
-	*/ 
-	public function testCreateIssueObjectFromJsonString($jsonString)
-    {
-    	$label = Issue::parseJsonString($jsonString);
-    	$this->assertEquals($jsonString, $label->toJsonString());
+    public function getJsonIssues(){
+        $data = [
+            [file_get_contents('./responses/issue14815.json'), "14815", "PLAN-572", "Cálculo correcto del VAN"],
+        ];
+        return $data;
     }
     
     /**
-	* @dataProvider getJsonIssues
-	*/ 
-	public function testIssueFieldsId($jsonString, $issueId, $issueKey, $issueSummary)
+    * @dataProvider getJsonIssues
+    */ 
+    public function testCreateIssueObjectFromJsonString($jsonString)
     {
-    	$label = Issue::parseJsonString($jsonString);
-    	$this->assertEquals($issueId, $label->getId());
-    	$this->assertEquals($issueKey, $label->getKey());
-    	$this->assertEquals($issueKey, $label->getKey());
-    	$this->assertEquals($issueSummary, $label->getSummary());
+        $label = Issue::parseJsonString($jsonString);
+        $this->assertEquals($jsonString, $label->toJsonString());
+    }
+    
+    /**
+    * @dataProvider getJsonIssues
+    */ 
+    public function testIssueFieldsId($jsonString, $issueId, $issueKey, $issueSummary)
+    {
+        $label = Issue::parseJsonString($jsonString);
+        $this->assertEquals($issueId, $label->getId());
+        $this->assertEquals($issueKey, $label->getKey());
+        $this->assertEquals($issueKey, $label->getKey());
+        $this->assertEquals($issueSummary, $label->getSummary());
     }
 }
